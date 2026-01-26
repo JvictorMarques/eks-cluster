a9af96b24320fac214081e28f4911051ac25315a
JvictorMarques <jvictorsilvamarques358@gmail.com>
2026-01-26 14:07:31 -0300

    feat: Update Terraform configuration for EKS cluster with VPC and subnet resources

--- variables.tf
+++ variables.tf
@@ -1,7 +1,32 @@
-variable "tags" {
+variable "default_tags" {
   type        = map(string)
   description = "Default tags for the eks-cluster project"
   default = {
-    Project = "eks-cluster"
+    Project   = "eks-cluster"
+    ManagedBy = "Terraform"
+  }
+}
+
+variable "vpc" {
+  type = object({
+    tags       = map(string)
+    cidr_block = string
+  })
+  description = "Values from VPC configuration"
+  default = {
+    tags = {
+      Name = "eks-cluster-vpc"
+    }
+    cidr_block = "10.0.0.0/16"
+  }
+}
+
+variable "subnets" {
+  type = object({
+    availability_zone = list(string)
+  })
+  description = "Values from Subnets public and private configuration"
+  default = {
+    availability_zone = ["us-east-1a", "us-east-1b"]
   }
 }
